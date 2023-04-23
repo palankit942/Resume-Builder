5c85b3ceede3724d935054751a7496f96a2e1269
palankit942 <palankit942@gmail.com>
2023-04-23 09:35:18 +0530

    changes

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,7 @@ import { Avatar, Box, chakra, Flex, Icon, SimpleGrid, useColorModeValue, } from 
 const testimonials = [
     {
         name: 'Ankit Kumar Pal',
-        role: 'Fronted Web Developer',
+        role: 'Fronted Developer',
         content:
             'A resume builder website is a web-based tool that allows users to create and customize a professional resume to their desired specifications. These websites typically provide templates for creating a resume.',
         avatar:
